4793020e46e9f31441efbb9320a4f10ccd3d4d9d
Maximilian Strunk <maxi.overwatch.esport@gmail.com>
2025-09-15 13:14:20 +0200

    nodemon itd

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
+//inicjujemy prosty serwer http za pomoca express.js
 const express = require('express');
-
+//tworzymy aplikacje express - instancje
 const app = express();
-
+//definiujemy prosta trase get na sciezce "/"
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
-
+//uruchamiamy serwer na porcie 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
